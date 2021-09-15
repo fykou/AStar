113fb90c3512ad4a5c1b037a35b915bfb8bee65b
Hoeyby <alex.hoeyby@gmail.com>
2021-09-15 14:00:46 +0200

    Setup

---
+++ main.py
@@ -0,0 +1,18 @@
+from map import Map_Obj
+from aStar import aStar
+
+
+
+# Testing:
+task1 = Map_Obj(1)
+task2 = Map_Obj(2)
+task3 = Map_Obj(3)
+task4 = Map_Obj(4)
+task5 = Map_Obj(5)
+
+# obj.read_map(obj, "")
+task1.show_map()
+task2.show_map()
+task3.show_map()
+task4.show_map()
+task5.show_map()
